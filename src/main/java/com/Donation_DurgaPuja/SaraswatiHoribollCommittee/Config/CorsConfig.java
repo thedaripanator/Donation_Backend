2dfdd7d94f6fde93fed0c570bacf6c4bab0d3040
Sayondeep Daripa <sayondeep1618@gmail.com>
2026-05-12 15:21:51 +0530

    Update CorsConfig.java

--- src/main/java/com/Donation_DurgaPuja/SaraswatiHoribollCommittee/Config/CorsConfig.java
+++ src/main/java/com/Donation_DurgaPuja/SaraswatiHoribollCommittee/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://donation-frontend-mauve.vercel.app","https://saraswatihoribollcommittee.vercel.app/")
+                        .allowedOrigins("http://localhost:5173","https://donation-frontend-mauve.vercel.app","https://saraswatihoribollcommittee.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
